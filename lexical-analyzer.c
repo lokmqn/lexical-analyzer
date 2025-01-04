79b0d59f5fbb856fffc084283dfa489cc0b8bfc8
polarstar 𒉭 <137574070+lokmqn@users.noreply.github.com>
2025-01-05 06:12:56 +0900

    Update lexical-analyzer.c

--- lexical-analyzer.c
+++ lexical-analyzer.c
@@ -4,7 +4,7 @@
 
 #define MAX_TOKEN_LENGTH 100
 
-// token types
+// Token types
 typedef enum {
     TOKEN_KEYWORD,
     TOKEN_IDENTIFIER,
@@ -14,11 +14,11 @@ typedef enum {
     TOKEN_UNKNOWN
 } TokenType;
 
-// keywords
+//keywords
 const char *keywords[] = {"if", "else", "while", "return", "int", "float", "void"};
 const int num_keywords = sizeof(keywords) / sizeof(keywords[0]);
 
-// Check if a word is a keyword
+
 int isKeyword(const char *word) {
     for (int i = 0; i < num_keywords; i++) {
         if (strcmp(word, keywords[i]) == 0) {
@@ -28,6 +28,7 @@ int isKeyword(const char *word) {
     return 0;
 }
 
+
 void printToken(TokenType type, const char *value) {
     const char *type_str[] = {
         "Keyword", "Identifier", "Number", "Operator", "Delimiter", "Unknown"
@@ -35,117 +36,85 @@ void printToken(TokenType type, const char *value) {
     printf("%s: %s\n", type_str[type], value);
 }
 
-void lexicalAnalyzer(const char *source) {
-    int i = 0;
+
+void analyzeToken(const char *input) {
     char token[MAX_TOKEN_LENGTH];
-    int tokenIndex = 0;
+    int i = 0;
+    int length = strlen(input);
 
-    enum { START, IN_IDENTIFIER, IN_NUMBER, IN_OPERATOR, IN_DELIMITER } state = START;
+    while (i < length) {
+        char current = input[i];
 
-    while (source[i] != '\0') {
-        char current = source[i];
-
-        switch (state) {
-            case START:
-                if (isalpha(current)) {
-                    state = IN_IDENTIFIER;
-                    token[tokenIndex++] = current;
-                } else if (isdigit(current)) {
-                    state = IN_NUMBER;
-                    token[tokenIndex++] = current;
-                } else if (strchr("+-*/=", current)) {
-                    state = IN_OPERATOR;
-                    token[tokenIndex++] = current;
-                } else if (strchr(";,(){}", current)) {
-                    state = IN_DELIMITER;
-                    token[tokenIndex++] = current;
-                } else if (isspace(current)) {
-                    // Ignore whitespace
-                } else {
-                    token[tokenIndex++] = current;
-                    printToken(TOKEN_UNKNOWN, token);
-                    tokenIndex = 0;
-                }
-                break;
-
-            case IN_IDENTIFIER:
-                if (isalnum(current)) {
-                    token[tokenIndex++] = current;
-                } else {
-                    token[tokenIndex] = '\0';
-                    state = START;
-                    if (isKeyword(token)) {
-                        printToken(TOKEN_KEYWORD, token);
-                    } else {
-                        printToken(TOKEN_IDENTIFIER, token);
-                    }
-                    tokenIndex = 0;
-                    continue; 
-                }
-                break;
-
-            case IN_NUMBER:
-                if (isdigit(current)) {
-                    token[tokenIndex++] = current;
-                } else {
-                    token[tokenIndex] = '\0';
-                    printToken(TOKEN_NUMBER, token);
-                    state = START;
-                    tokenIndex = 0;
-                    continue; 
-                }
-                break;
-
-            case IN_OPERATOR:
-                token[tokenIndex] = '\0';
-                printToken(TOKEN_OPERATOR, token);
-                state = START;
-                tokenIndex = 0;
-                continue; 
-                break;
-
-            case IN_DELIMITER:
-                token[tokenIndex] = '\0';
-                printToken(TOKEN_DELIMITER, token);
-                state = START;
-                tokenIndex = 0;
-                continue;
-                break;
+        if (isalpha(current)) {
+            int j = 0;
+            while (i < length && (isalnum(input[i]) || input[i] == '_')) {
+                token[j++] = input[i++];
+            }
+            token[j] = '\0';
+            if (isKeyword(token)) {
+                printToken(TOKEN_KEYWORD, token);
+            } else {
+                printToken(TOKEN_IDENTIFIER, token);
+            }
         }
-        i++;
-    }
 
-    if (tokenIndex > 0) {
-        token[tokenIndex] = '\0';
-        switch (state) {
-            case IN_IDENTIFIER:
-                if (isKeyword(token)) {
-                    printToken(TOKEN_KEYWORD, token);
-                } else {
-                    printToken(TOKEN_IDENTIFIER, token);
-                }
-                break;
-            case IN_NUMBER:
-                printToken(TOKEN_NUMBER, token);
-                break;
-            case IN_OPERATOR:
-                printToken(TOKEN_OPERATOR, token);
-                break;
-            case IN_DELIMITER:
-                printToken(TOKEN_DELIMITER, token);
-                break;
-            default:
-                printToken(TOKEN_UNKNOWN, token);
-                break;
+        else if (isdigit(current)) {
+            int j = 0;
+            while (i < length && isdigit(input[i])) {
+                token[j++] = input[i++];
+            }
+            token[j] = '\0';
+            printToken(TOKEN_NUMBER, token);
+        }
+
+        else if (strchr("+-*/=><", current)) {
+            token[0] = current;
+            token[1] = '\0';
+            printToken(TOKEN_OPERATOR, token);
+            i++;
+        }
+
+        else if (strchr(";,(){}[]", current)) {
+            token[0] = current;
+            token[1] = '\0';
+            printToken(TOKEN_DELIMITER, token);
+            i++;
+        }
+
+        else if (isspace(current)) {
+            i++;
+        }
+
+        else {
+            token[0] = current;
+            token[1] = '\0';
+            printToken(TOKEN_UNKNOWN, token);
+            i++;
         }
     }
 }
 
 int main() {
-    const char *source_code = "int main() { int a = 10; if (a > 5) return a; }";
+    char input[MAX_TOKEN_LENGTH];
 
-    printf("Tokenizing the source code:\n");
-    lexicalAnalyzer(source_code);
+    printf("Lexical Analyzer - Type input and get token types (type 'exit' to quit):\n");
+
+    while (1) {
+        printf("\nEnter input: ");
+        fgets(input, MAX_TOKEN_LENGTH, stdin);
+
+
+        input[strcspn(input, "\n")] = '\0';
+
+
+        if (strcmp(input, "exit") == 0) {
+            printf("Exiting the program.\n");
+            break;
+        }
+
+
+        analyzeToken(input);
+    }
 
     return 0;
 }
